af32086d971ceccd8a1202237ac3533dc6cb89a4
nibin007 <89572613+nibin007@users.noreply.github.com>
2023-03-13 15:44:12 +0530

    Add files via upload

---
+++ Product.java
@@ -0,0 +1,26 @@
+class Prod{
+   String pcode;
+   String name;
+   int price;
+   public Product (){
+               System.out.println("enter price : ");
+   
+   
+   
+   }
+   
+  
+
+
+}
+class Product{
+   public static void main(String []args) {
+           Prod Obj=new Prod();
+                 
+   
+   
+   }
+
+
+
+}
